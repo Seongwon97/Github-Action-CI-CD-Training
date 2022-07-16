efb30b2ec22d8db364fbcb830b1bbf5c01fffcb8
Seongwon97 <osw970325@naver.com>
2022-07-17 02:14:03 +0900

    feat: 줄바꿈 추가

--- backend/src/main/java/com/woowacourse/momo/auth/controller/AuthController.java
+++ backend/src/main/java/com/woowacourse/momo/auth/controller/AuthController.java
@@ -27,6 +27,7 @@ public class AuthController {
     public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
         LoginResponse response = authService.login(request);
 
+
         return ResponseEntity.ok(response);
     }
 
