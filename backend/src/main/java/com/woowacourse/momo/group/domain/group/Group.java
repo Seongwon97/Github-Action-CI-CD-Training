e90dcb1f0e7f7bb1b21cbea1132295865d955d82
Seongwon97 <osw970325@naver.com>
2022-07-20 16:47:15 +0900

    update backend-pr-ci.yml

--- backend/src/main/java/com/woowacourse/momo/group/domain/group/Group.java
+++ backend/src/main/java/com/woowacourse/momo/group/domain/group/Group.java
@@ -18,6 +18,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import lombok.AccessLevel;
@@ -66,6 +67,7 @@ public class Group {
     private List<Schedule> schedules = new ArrayList<>();
 
     @Column(nullable = false)
+    @ManyToOne
     private String location;
 
     @Lob
